8f4b045eda24b0153651d616dd5595c0a912753f
Aleksei Arro <polsck@gmail.com>
2018-10-09 16:30:43 +0300

    SVG improved

--- source/js/editor-range.js
+++ source/js/editor-range.js
@@ -2,8 +2,8 @@ var buttonCrop = document.querySelector('.editor__toggle-button--crop');
 var buttonColor = document.querySelector('.editor__toggle-button--color');
 var buttonContrast = document.querySelector('.editor__toggle-button--contrast');
 var pathCrop = document.querySelector('.editor__path--crop');
-var pathColor = document.querySelectorAll('.editor__path--color');
-var pathContrast = document.querySelectorAll('.editor__path--contrast');
+var pathColor = document.querySelector('.editor__path--color');
+var pathContrast = document.querySelector('.editor__path--contrast');
 var rangeCrop = document.querySelector('.editor__item--crop');
 var rangeColor = document.querySelector('.editor__item--color');
 var rangeContrast = document.querySelector('.editor__item--contrast');
@@ -14,10 +14,8 @@ buttonCrop.addEventListener('click', function(evt) {
   rangeColor.classList.add('range-hidden');
   rangeContrast.classList.add('range-hidden');
   pathCrop.classList.add('editor-active-toggle');
-  for (var i = 0; i <= 1; i++) {
-    pathColor[i].classList.remove('editor-active-toggle');
-    pathContrast[i].classList.remove('editor-active-toggle');
-  }
+  pathColor.classList.remove('editor-active-toggle');
+  pathContrast.classList.remove('editor-active-toggle');
 });
 
 buttonColor.addEventListener('click', function(evt) {
@@ -26,10 +24,8 @@ buttonColor.addEventListener('click', function(evt) {
   rangeColor.classList.remove('range-hidden');
   rangeContrast.classList.add('range-hidden');
   pathCrop.classList.remove('editor-active-toggle');
-  for (var i = 0; i <= 1; i++) {
-    pathColor[i].classList.add('editor-active-toggle');
-    pathContrast[i].classList.remove('editor-active-toggle');
-  }
+  pathColor.classList.add('editor-active-toggle');
+  pathContrast.classList.remove('editor-active-toggle');
 });
 
 buttonContrast.addEventListener('click', function(evt) {
@@ -38,8 +34,6 @@ buttonContrast.addEventListener('click', function(evt) {
   rangeColor.classList.add('range-hidden');
   rangeContrast.classList.remove('range-hidden');
   pathCrop.classList.remove('editor-active-toggle');
-  for (var i = 0; i <= 1; i++) {
-    pathColor[i].classList.remove('editor-active-toggle');
-    pathContrast[i].classList.add('editor-active-toggle');
-  }
+  pathColor.classList.remove('editor-active-toggle');
+  pathContrast.classList.add('editor-active-toggle');
 });
