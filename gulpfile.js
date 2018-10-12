994d9775abf0e4be19054cc78e4c2696d9e867f8
Aleksei Arro <polsck@gmail.com>
2018-10-12 19:31:34 +0300

    Automation test

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 // Variables
 
+// TODO: подумать, все ли опции нужны
+
 var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
 var del = require('del');
@@ -13,10 +15,25 @@ var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var posthtml = require('gulp-posthtml');
 var rename = require('gulp-rename');
-var run = require('run-sequence'); //maybe need to delete at final development stage
+var webp = require('gulp-webp');
 var sass = require('gulp-sass');
 
 // Task functions
+
+var del = function () {
+  return del('build');
+}
+
+var copy = function () {
+  return gulp.src([
+    'source/fonts/**/*.{woff,woff2}',
+    'source/img/**',
+    'source/js/**'
+  ]), {
+    base: 'source'
+  }
+    .pipe(gulp.dest('build'));
+}
 
 var style = function () {
   return gulp.src('source/sass/style.scss')
@@ -25,24 +42,36 @@ var style = function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest('source/css/'))
+    .pipe(gulp.dest('build/css/'))
+    .pipe(minify())
+    .pipe(remane('style.min.css'))
+    .pipe(gulp.dest('build/css/'))
     .pipe(browserSync.stream());
+}
+
+var html = function () {
+  return gulp.src('source/*.html')
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest('build'));
 }
 
 var serve = function () {
   browserSync.init({
-    server: 'source/',
+    server: 'build/',
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
-  gulp.watch('source/sass/**/*.scss', style);
-  gulp.watch('source/js/**/*.js').on('change', browserSync.reload);
-  gulp.watch('source/*.html').on('change', browserSync.reload);
+  gulp.watch('build/sass/**/*.scss', style);
+  gulp.watch('build/js/**/*.js').on('change', browserSync.reload);
+  gulp.watch('build/img/**/*.svg').on('change', browserSync.reload);
+  gulp.watch('build/*.html').on('change', browserSync.reload);
 }
 
 // Gulp tasks
 
-gulp.task('build', style);
+gulp.task('build', gulp.series(del, copy, style, html));
 gulp.task('serve', serve);
